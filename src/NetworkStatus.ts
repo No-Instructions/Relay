ef3286c69f62f5f9b683da5417ac6a1b13f2f8c7
Daniel Grossmann-Kavanagh <me@danielgk.com>
2024-04-15 16:32:01 -0700

    chore: Add start/stop controls to NetworkStatus class

--- src/NetworkStatus.ts
+++ src/NetworkStatus.ts
@@ -8,17 +8,30 @@ class NetworkStatus {
 	private onOnline: Callback[] = [];
 	private _onceOnline: Set<Callback>;
 	private onOffline: Callback[] = [];
+	private timer: NodeJS.Timer;
 	online = true;
 
 	constructor(url: string, interval = 10000) {
 		this.url = url;
 		this.interval = interval;
 		this._onceOnline = new Set();
-		this.checkStatusRepeatedly();
+		this.start();
 	}
 
-	private checkStatusRepeatedly(): void {
-		setInterval(this._checkStatus.bind(this), this.interval);
+	public start() {
+		if (!this.timer) {
+			this.timer = this.checkStatusRepeatedly();
+		}
+	}
+
+	public stop() {
+		if (this.timer) {
+			clearInterval(this.timer);
+		}
+	}
+
+	private checkStatusRepeatedly(): NodeJS.Timer {
+		return setInterval(this._checkStatus.bind(this), this.interval);
 	}
 
 	public checkStatus(): Promise<boolean> {
