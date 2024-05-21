d28edc0b19a7e9bd84d3549924938159a02c583b
Daniel Grossmann-Kavanagh <me@danielgk.com>
2024-05-21 09:51:30 -0700

    feat: Add connection status to folder navigation

--- src/ui/FolderNav.ts
+++ src/ui/FolderNav.ts
@@ -1,6 +1,7 @@
-import { Workspace } from "obsidian";
-import { SharedFolders } from "../SharedFolder";
+import { TFolder, Workspace, WorkspaceLeaf } from "obsidian";
+import { SharedFolder, SharedFolders } from "../SharedFolder";
 import { VaultFacade } from "src/obsidian-api/Vault";
+import type { ConnectionState } from "src/HasProvider";
 
 export class FolderNavIcon {
 	vault: VaultFacade;
@@ -25,6 +26,51 @@ export class FolderNavIcon {
 		return this.workspace.on("layout-change", () => this.refresh());
 	}
 
+	folderStatus(el: any, folder?: SharedFolder) {
+		if (folder?.state.status === "connected") {
+			el.nextSibling?.removeClass("system3-connecting");
+			el.nextSibling?.addClass("system3-connected");
+		} else if (folder?.state.status === "connecting") {
+			el.nextSibling?.removeClass("system3-connected");
+			el.nextSibling?.addClass("system3-connecting");
+		} else {
+			el.nextSibling?.removeClass("system3-connecting");
+			el.nextSibling?.removeClass("system3-connected");
+		}
+	}
+
+	docStatus(el: any, status?: ConnectionState) {
+		if (status?.status === "connected") {
+			el.removeClass("system3-connecting");
+			el.addClass("system3-connected");
+		} else if (status?.status === "connecting") {
+			el.removeClass("system3-connected");
+			el.addClass("system3-connecting");
+		} else {
+			el.removeClass("system3-connected");
+			el.removeClass("system3-connecting");
+		}
+	}
+
+	removeStatuses(fileExplorer: WorkspaceLeaf, folder: TFolder) {
+		//@ts-expect-error
+		const folderItem = fileExplorer.view.fileItems[folder.path];
+		this.folderStatus(folderItem.selfEl);
+		folder.children.forEach((child) => {
+			if (child instanceof TFolder) {
+				this.removeStatuses(fileExplorer, child);
+			} else {
+				//@ts-expect-error
+				const fileItem = fileExplorer.view.fileItems[child.path];
+				if (!fileItem) {
+					// likely a rename
+					return;
+				}
+				this.docStatus(fileItem.el);
+			}
+		});
+	}
+
 	refresh() {
 		const fileExplorers = this.workspace.getLeavesOfType("file-explorer");
 
@@ -35,22 +81,65 @@ export class FolderNavIcon {
 						return f.path == folder.path;
 					});
 					//@ts-expect-error
-					const fileItem = fileExplorer.view.fileItems[folder.path];
-					if (!fileItem) {
+					const folderItem = fileExplorer.view.fileItems[folder.path];
+					if (!folderItem) {
 						return;
 					}
-					const titleEl = fileItem.selfEl;
+					const titleEl = folderItem.selfEl;
 					let pill = titleEl.querySelector(".obsidian-live-pill");
 
-					if (sharedFolder && !pill) {
-						// add a pill
-						pill = titleEl.createDiv();
-						pill.classList.add("obsidian-live-pill");
-						pill.innerHTML = "<span>live</span>";
-						titleEl.appendChild(pill);
-					} else if (pill && !sharedFolder) {
+					if (sharedFolder) {
+						titleEl.addEventListener("click", () => {
+							if (this.sharedFolders.has(sharedFolder)) {
+								this.folderStatus(titleEl, sharedFolder);
+							}
+						});
+
+						sharedFolder.docs.forEach((doc) => {
+							const docPath = sharedFolder.getPath(doc.path);
+							const fileItem =
+								//@ts-expect-error
+								fileExplorer.view.fileItems[docPath];
+							if (!fileItem) {
+								// like a rename / race condition
+								return;
+							}
+							this.docStatus(fileItem.el, doc.state);
+							doc.subscribe(fileItem.el, (status) => {
+								const fileExplorers =
+									this.workspace.getLeavesOfType(
+										"file-explorer"
+									);
+								fileExplorers.forEach((fileExplorer) => {
+									const fileItem =
+										//@ts-expect-error
+										fileExplorer.view.fileItems[docPath];
+									if (!fileItem) {
+										// like a rename / race condition
+										return;
+									}
+									this.docStatus(fileItem.el, status);
+								});
+							});
+						});
+
+						this.folderStatus(titleEl, sharedFolder);
+
+						if (!pill) {
+							sharedFolder.subscribe(titleEl, (status) => {
+								this.folderStatus(titleEl, sharedFolder);
+							});
+
+							// add a pill
+							pill = titleEl.createDiv();
+							pill.classList.add("obsidian-live-pill");
+							pill.innerHTML = "<span>live</span>";
+							titleEl.appendChild(pill);
+						}
+					} else if (pill) {
 						// remove a pill
 						pill.remove();
+						this.removeStatuses(fileExplorer, folder);
 					}
 				}
 			});
