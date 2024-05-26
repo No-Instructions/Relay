64f3e49a1441e8d98fb2b557db6915f08aeaf496
Daniel Grossmann-Kavanagh <me@danielgk.com>
2024-05-26 02:43:21 -0700

    refactor: FolderNav now uses Visitor pattern

--- src/ui/FolderNav.ts
+++ src/ui/FolderNav.ts
@@ -1,16 +1,340 @@
-import { TAbstractFile, TFolder, Workspace, WorkspaceLeaf } from "obsidian";
+import {
+	TAbstractFile,
+	TFile,
+	TFolder,
+	Workspace,
+	WorkspaceLeaf,
+} from "obsidian";
 import { SharedFolder, SharedFolders } from "../SharedFolder";
 import { VaultFacade } from "src/obsidian-api/Vault";
-import type { ConnectionState, ConnectionStatus } from "src/HasProvider";
+import type { ConnectionState } from "src/HasProvider";
+import type { Document } from "src/Document";
 import Pill from "src/components/Pill.svelte";
+
+class SiblingWatcher {
+	mutationObserver: MutationObserver | null;
+	el: HTMLElement;
+
+	constructor(el: HTMLElement, onceSibling: (el: HTMLElement) => void) {
+		this.el = el;
+
+		const observer = new MutationObserver((mutationsList, observer) => {
+			for (let mutation of mutationsList) {
+				if (mutation.type === "childList") {
+					if (el.nextSibling) {
+						onceSibling(el);
+						observer.disconnect();
+					}
+				}
+			}
+		});
+		observer.observe(el.parentElement as HTMLElement, {
+			childList: true,
+			subtree: true,
+		});
+		this.mutationObserver = observer;
+	}
+
+	destroy() {
+		this.mutationObserver?.disconnect();
+		this.mutationObserver = null;
+	}
+}
+
+class FolderBar {
+	el: HTMLElement;
+	sharedFolder: SharedFolder;
+	siblingWatcher: SiblingWatcher;
+
+	constructor(el: HTMLElement, sharedFolder: SharedFolder) {
+		this.el = el;
+		this.sharedFolder = sharedFolder;
+		this.siblingWatcher = new SiblingWatcher(this.el, (el) => {
+			this.add();
+		});
+		this.add();
+	}
+
+	add() {
+		(this.el.nextSibling as HTMLElement)?.addClass("system3-live");
+	}
+
+	remove() {
+		(this.el.nextSibling as HTMLElement)?.removeClass("system3-live");
+	}
+
+	destroy() {
+		this.siblingWatcher.destroy();
+		this.remove();
+	}
+}
+
+interface TreeNode {
+	el: HTMLElement;
+}
+
+interface FileItem extends TreeNode {
+	el: HTMLElement;
+}
+
+interface FolderItem extends TreeNode {
+	el: HTMLElement;
+	selfEl: HTMLElement;
+}
+
+interface FileSystemVisitor<T> {
+	visitFolder(
+		folder: TFolder,
+		item: FolderItem,
+		storage?: T,
+		sharedFolder?: SharedFolder
+	): T | null;
+	visitFile(
+		file: TFile,
+		item: FileItem,
+		storage?: T,
+		sharedFolder?: SharedFolder
+	): T | null;
+}
+
+class BaseVisitor<T> implements FileSystemVisitor<T> {
+	visitFolder(
+		folder: TFolder,
+		item: FolderItem,
+		storage?: T,
+		sharedFolder?: SharedFolder
+	): T | null {
+		// do nothing
+		return null;
+	}
+
+	visitFile(
+		file: TFile,
+		item: FileItem,
+		storage: T,
+		sharedFolder: SharedFolder
+	): T | null {
+		// do nothing
+		return null;
+	}
+}
+
+class FolderBarVisitor extends BaseVisitor<FolderBar> {
+	visitFolder(
+		folder: TFolder,
+		item: FolderItem,
+		storage: FolderBar,
+		sharedFolder?: SharedFolder
+	) {
+		if (sharedFolder) {
+			return storage || new FolderBar(item.selfEl, sharedFolder);
+		}
+		if (storage) {
+			storage.destroy();
+		}
+		return null;
+	}
+}
+
+type Unsubscribe = () => void;
+
+class FolderPillVisitor extends BaseVisitor<[Pill, Unsubscribe]> {
+	visitFolder(
+		folder: TFolder,
+		item: FolderItem,
+		storage?: [Pill, Unsubscribe] | null,
+		sharedFolder?: SharedFolder
+	): [Pill, Unsubscribe] | null {
+		const titleEl = item.selfEl;
+		if (sharedFolder && !storage) {
+			const pill = new Pill({
+				target: titleEl,
+				props: {
+					status: sharedFolder.state.status,
+				},
+			});
+			const unsubscribe = sharedFolder.subscribe(titleEl, (status) => {
+				pill?.$set({ status: status.status });
+			});
+			return [pill, unsubscribe];
+		} else if (storage && !sharedFolder) {
+			const [pill, unsubscribe] = storage;
+			unsubscribe();
+			pill.$destroy();
+			return null;
+		} else if (storage && sharedFolder) {
+			return storage;
+		}
+		return null;
+	}
+}
+
+class DocumentStatus {
+	el: HTMLElement;
+	document?: Document;
+
+	constructor(el: HTMLElement, document: Document, doc: TFile) {
+		this.el = el;
+		this.document = document;
+		this.document.subscribe(el, (status) => {
+			this.docStatus(status);
+		});
+		this.docStatus(this.document.state);
+	}
+
+	docStatus(status?: ConnectionState) {
+		if (status?.status === "connected") {
+			this.el.removeClass("system3-connecting");
+			this.el.addClass("system3-connected");
+			this.el.addClass("system3-live");
+		} else if (status?.status === "connecting") {
+			this.el.removeClass("system3-connected");
+			this.el.addClass("system3-connecting");
+			this.el.addClass("system3-live");
+		} else if (status?.status === "disconnected") {
+			this.el.addClass("system3-live");
+			this.el.removeClass("system3-connected");
+			this.el.removeClass("system3-connecting");
+		} else {
+			this.el.removeClass("system3-connected");
+			this.el.removeClass("system3-connecting");
+			this.el.removeClass("system3-live");
+		}
+	}
+
+	destroy() {
+		this.document?.unsubscribe(this.el);
+		this.docStatus();
+	}
+}
+
+class FileStatusVisitor extends BaseVisitor<DocumentStatus> {
+	visitFile(
+		file: TFile,
+		item: FileItem,
+		storage?: DocumentStatus,
+		sharedFolder?: SharedFolder
+	): DocumentStatus | null {
+		if (sharedFolder) {
+			const document = sharedFolder.getFile(file.path, false);
+			if (!document) {
+				return null;
+			}
+			return storage || new DocumentStatus(item.el, document, file);
+		}
+		if (storage) {
+			storage.destroy();
+		}
+		return null;
+	}
+}
+
+class FileExplorerWalker {
+	fileExplorer: WorkspaceLeaf;
+	sharedFolders: SharedFolders;
+	visitors: FileSystemVisitor<any>[];
+	storage: Map<FileSystemVisitor<any>, Map<TreeNode, any>>;
+
+	constructor(
+		fileExplorer: WorkspaceLeaf,
+		sharedFolders: SharedFolders,
+		visitors: FileSystemVisitor<any>[]
+	) {
+		this.fileExplorer = fileExplorer;
+		this.sharedFolders = sharedFolders;
+		this.visitors = visitors;
+
+		this.storage = new Map<FileSystemVisitor<any>, Map<TreeNode, any>>();
+		for (const visitor of this.visitors) {
+			this.storage.set(visitor, new Map<TreeNode, any>());
+		}
+	}
+
+	private _getFileExplorerItem(path: string) {
+		// XXX this is a private API
+		//@ts-expect-error this is a private API
+		return this.fileExplorer.view.fileItems[path];
+	}
+	private getFileExplorerItem<T>(
+		fileExplorer: WorkspaceLeaf,
+		file: string
+	): T;
+
+	private getFileExplorerItem<T>(
+		fileExplorer: WorkspaceLeaf,
+		file: TAbstractFile
+	): T;
+
+	// XXX this is a private API
+	private getFileExplorerItem<T>(
+		fileExplorer: WorkspaceLeaf,
+		fileOrFolder: TAbstractFile | string
+	) {
+		if (typeof fileOrFolder === "string") {
+			return this._getFileExplorerItem(fileOrFolder) as T;
+		}
+		return this._getFileExplorerItem(fileOrFolder.path) as T;
+	}
+
+	walk(folder: TFolder) {
+		const sharedFolder = this.sharedFolders.find(
+			(sharedFolder) => sharedFolder.path === folder.path
+		);
+		const folderItem = this.getFileExplorerItem<FolderItem>(
+			this.fileExplorer,
+			folder
+		);
+		if (folderItem) {
+			// XXX cache this
+			this.storage.forEach((store, visitor) => {
+				const stored = store.get(folderItem);
+				const update = visitor.visitFolder(
+					folder,
+					folderItem,
+					stored,
+					sharedFolder
+				);
+				if (stored && !update) {
+					store.delete(folderItem);
+				} else if (update) {
+					store.set(folderItem, update);
+				}
+			});
+		}
+		folder.children.forEach((child) => {
+			if (child instanceof TFolder) {
+				this.walk(child);
+			} else if (child instanceof TFile) {
+				const fileItem = this.getFileExplorerItem<FileItem>(
+					this.fileExplorer,
+					child
+				);
+				this.storage.forEach((store, visitor) => {
+					const stored = store.get(fileItem);
+					const update = visitor.visitFile(
+						child,
+						fileItem,
+						stored,
+						sharedFolder
+					);
+					if (stored && !update) {
+						store.delete(fileItem);
+					} else if (update) {
+						store.set(fileItem, update);
+					}
+				});
+			}
+		});
+	}
+}
 
 export class FolderNavigationDecorations {
 	vault: VaultFacade;
 	workspace: Workspace;
 	sharedFolders: SharedFolders;
 	folderListener: any;
-	mutationObservers: Map<HTMLElement, any>;
 	pills: Map<HTMLElement, Pill>;
+	treeState: Map<WorkspaceLeaf, FileExplorerWalker>;
 
 	constructor(
 		vault: VaultFacade,
@@ -19,260 +343,60 @@ export class FolderNavigationDecorations {
 	) {
 		this.vault = vault;
 		this.pills = new Map<HTMLElement, Pill>();
-		this.mutationObservers = new Map<HTMLElement, any>();
 		this.workspace = workspace;
 		this.sharedFolders = sharedFolders;
-
+		this.treeState = new Map<WorkspaceLeaf, FileExplorerWalker>();
 		this.workspace.onLayoutReady(() => this.refresh());
 		this.folderListener = this.sharedFolders.on(() => this.refresh());
 		this.refresh();
 	}
 
 	register() {
-		return this.workspace.on("layout-change", () => this.refresh());
+		return this.workspace.on("layout-change", () => this.quickRefresh());
 	}
 
-	folderDecoration(el: any, isSharedFolder: boolean) {
-		if (isSharedFolder) {
-			el.nextSibling?.addClass("system3-live");
-		} else {
-			el.nextSibling?.removeClass("system3-live");
-		}
-	}
-	//	if (status === "connected") {
-	//		el.nextSibling?.removeClass("system3-connecting");
-	//		el.nextSibling?.addClass("system3-live");
-	//		el.nextSibling?.addClass("system3-connected");
-	//	} else if (status === "connecting") {
-	//		el.nextSibling?.removeClass("system3-connected");
-	//		el.nextSibling?.addClass("system3-live");
-	//		el.nextSibling?.addClass("system3-connecting");
-	//	} else if (status === "disconnected" || status === "unknown") {
-	//		el.nextSibling?.removeClass("system3-connected");
-	//		el.nextSibling?.removeClass("system3-connecting");
-	//		el.nextSibling?.addClass("system3-live");
-	//	} else {
-	//		el.nextSibling?.removeClass("system3-connecting");
-	//		el.nextSibling?.removeClass("system3-connected");
-	//		el.nextSibling?.removeClass("system3-live");
-	//	}
-	//}
-
-	docStatus(el: any, status?: ConnectionState) {
-		if (status?.status === "connected") {
-			el.removeClass("system3-connecting");
-			el.addClass("system3-connected");
-			el.addClass("system3-live");
-		} else if (status?.status === "connecting") {
-			el.removeClass("system3-connected");
-			el.addClass("system3-connecting");
-			el.addClass("system3-live");
-		} else if (status?.status === "disconnected") {
-			el.addClass("system3-live");
-			el.removeClass("system3-connected");
-			el.removeClass("system3-connecting");
-		} else {
-			el.removeClass("system3-connected");
-			el.removeClass("system3-connecting");
-			el.removeClass("system3-live");
-		}
-	}
-
-	private getFileExplorerItem(fileExplorer: WorkspaceLeaf, file: string): any;
-
-	private getFileExplorerItem(
-		fileExplorer: WorkspaceLeaf,
-		file: TAbstractFile
-	): any;
-
-	private getFileExplorerItem(
-		fileExplorer: WorkspaceLeaf,
-		file: TAbstractFile | string
-	) {
-		// XXX this is a private API
-		const path = file instanceof TAbstractFile ? file.path : file;
-		//@ts-expect-error
-		return fileExplorer.view.fileItems[path];
-	}
-
-	removePills(force = false, folder?: TFolder) {
-		const sharedFolderPaths = force
-			? []
-			: this.sharedFolders.map((sharedFolder) => sharedFolder.path);
+	quickRefresh() {
 		const fileExplorers = this.workspace.getLeavesOfType("file-explorer");
-		const root = folder
-			? folder
-			: (this.vault.getAbstractFileByPath("/") as TFolder | null);
-		console.warn("remove pills", sharedFolderPaths, fileExplorers, root);
-		fileExplorers.forEach((fileExplorer) => {
-			if (root) {
-				this._removePills(fileExplorer, root, sharedFolderPaths);
-			}
-		});
-	}
-
-	_removePills(
-		fileExplorer: WorkspaceLeaf,
-		folder: TFolder,
-		sharedFolderPaths: string[]
-	) {
-		const folderItem = this.getFileExplorerItem(fileExplorer, folder);
-		if (folderItem) {
-			const pill = this.pills.get(folderItem.selfEl);
-			if (pill && !sharedFolderPaths.contains(folder.path)) {
-				console.warn("removing pill");
-				pill.$destroy();
-				this.pills.delete(folderItem.selfEl);
+		const sharedFolders = this.sharedFolders.map((folder) => folder.path);
+		for (const fileExplorer of fileExplorers) {
+			const walker =
+				this.treeState.get(fileExplorer) ||
+				new FileExplorerWalker(fileExplorer, this.sharedFolders, [
+					new FolderBarVisitor(),
+					new FolderPillVisitor(),
+					new FileStatusVisitor(),
+				]);
+			this.treeState.set(fileExplorer, walker);
+			for (const sharedFolderPath of sharedFolders) {
+				const sharedFolder =
+					this.vault.getAbstractFileByPath(sharedFolderPath);
+				if (sharedFolder instanceof TFolder) {
+					walker.walk(sharedFolder);
+				}
 			}
 		}
-		folder.children.forEach((child) => {
-			if (child instanceof TFolder) {
-				this._removePills(fileExplorer, child, sharedFolderPaths);
-			}
-		});
-	}
-
-	removeStatuses(force = false, folder?: TFolder) {
-		const sharedFolderPaths = force
-			? []
-			: this.sharedFolders.map((sharedFolder) => sharedFolder.path);
-		const fileExplorers = this.workspace.getLeavesOfType("file-explorer");
-		const root = folder
-			? folder
-			: (this.vault.getAbstractFileByPath("/") as TFolder | null);
-		fileExplorers.forEach((fileExplorer) => {
-			if (root) {
-				this._removeStatuses(fileExplorer, root, sharedFolderPaths);
-			}
-		});
-	}
-
-	_removeStatuses(
-		fileExplorer: WorkspaceLeaf,
-		folder: TFolder,
-		sharedFolderPaths: string[]
-	) {
-		const folderItem = this.getFileExplorerItem(fileExplorer, folder);
-		if (folderItem) {
-			const decorate = sharedFolderPaths.contains(folder.path);
-			if (decorate) {
-				console.warn("decorate", folder.path);
-			}
-			this.folderDecoration(folderItem.selfEl, decorate);
-		}
-		folder.children.forEach((child) => {
-			if (child instanceof TFolder) {
-				this._removeStatuses(fileExplorer, child, sharedFolderPaths);
-			} else {
-				const fileItem = this.getFileExplorerItem(fileExplorer, child);
-				this.docStatus(fileItem.el);
-			}
-		});
 	}
 
 	refresh() {
-		console.warn("foldernav refresh");
 		const fileExplorers = this.workspace.getLeavesOfType("file-explorer");
-		const sharedFolders = this.sharedFolders.items();
-
-		sharedFolders.forEach((sharedFolder) => {
-			console.warn("shared folder", sharedFolder.path);
-			const folder = this.vault.getAbstractFileByPath(
-				sharedFolder.path
-			) as TFolder | null;
-			if (!folder) {
-				return;
+		for (const fileExplorer of fileExplorers) {
+			const walker =
+				this.treeState.get(fileExplorer) ||
+				new FileExplorerWalker(fileExplorer, this.sharedFolders, [
+					new FolderBarVisitor(),
+					new FolderPillVisitor(),
+					new FileStatusVisitor(),
+				]);
+			this.treeState.set(fileExplorer, walker);
+			const root = this.vault.getAbstractFileByPath("/");
+			if (root instanceof TFolder) {
+				walker.walk(root);
 			}
-
-			fileExplorers.forEach((fileExplorer) => {
-				const folderItem = this.getFileExplorerItem(
-					fileExplorer,
-					folder
-				);
-				if (!folderItem) {
-					return;
-				}
-				const titleEl = folderItem.selfEl;
-				let pill = this.pills.get(titleEl);
-
-				if (!pill) {
-					pill = new Pill({
-						target: titleEl,
-						props: {
-							status: sharedFolder.state.status,
-						},
-					});
-					sharedFolder.subscribe(titleEl, (status) => {
-						pill?.$set({ status: status.status });
-					});
-					this.pills.set(titleEl, pill);
-				}
-
-				if (!this.mutationObservers.has(titleEl)) {
-					const observer = new MutationObserver(
-						(mutationsList, observer) => {
-							for (let mutation of mutationsList) {
-								if (mutation.type === "childList") {
-									if (titleEl.nextSibling) {
-										console.log("observation");
-										this.folderDecoration(titleEl, true);
-										pill?.$set({
-											status: sharedFolder.state.status,
-										});
-										observer.disconnect();
-									}
-								}
-							}
-						}
-					);
-					observer.observe(titleEl.parentElement, {
-						childList: true,
-						subtree: true,
-					});
-					this.mutationObservers.set(titleEl, observer);
-				}
-
-				this.folderDecoration(titleEl, true);
-
-				sharedFolder.whenReady().then(() => {
-					sharedFolder.docs.forEach((doc) => {
-						const docPath = sharedFolder.getPath(doc.path);
-						const fileItem = this.getFileExplorerItem(
-							fileExplorer,
-							docPath
-						);
-						this.docStatus(fileItem.el, doc.state);
-						doc.subscribe(fileItem.el, (status) => {
-							const fileExplorers =
-								this.workspace.getLeavesOfType("file-explorer");
-							fileExplorers.forEach((fileExplorer) => {
-								const fileItem = this.getFileExplorerItem(
-									fileExplorer,
-									docPath
-								);
-								this.docStatus(fileItem.el, status);
-							});
-						});
-					});
-				});
-			});
-		});
-		console.warn("cleanup");
-		this.removePills();
-		this.removeStatuses();
+		}
 	}
 
 	destroy() {
 		this.sharedFolders.off(this.folderListener);
-		const fileExplorers = this.workspace.getLeavesOfType("file-explorer");
-		this.removePills(true);
-		this.removeStatuses(true);
-		this.mutationObservers.forEach((listener, el) => {
-			listener.disconnect();
-		});
-		this.pills.forEach((pill) => {
-			pill.$destroy();
-		});
 		this.refresh();
 	}
 }
