8ebb7d9288f2c7352aee14428f933f1ab9de1e98
Daniel Grossmann-Kavanagh <me@danielgk.com>
2024-06-18 08:51:17 -0700

    types: fixup debug.ts types

--- src/debug.ts
+++ src/debug.ts
@@ -17,13 +17,11 @@ function toastProd(error: Error): Error {
 
 // Define two versions of curryLog
 function curryLogDebug(initialText: string) {
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	return (...args: any[]) => console.log(initialText, ": ", ...args);
+	return (...args: unknown[]) => console.log(initialText, ": ", ...args);
 }
 
 function curryLogProd(initialText: string) {
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	return (...args: any[]) => {};
+	return (...args: unknown[]) => {};
 }
 const debug = BUILD_TYPE === "debug";
 export const curryLog = debug ? curryLogDebug : curryLogProd;
