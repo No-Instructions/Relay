84fc18ff4be56fc5cd1cbeb08da8feb6b40f0769
Daniel Grossmann-Kavanagh <me@danielgk.com>
2024-09-04 12:00:21 -0700

    feat: Add feature flags

---
+++ src/flags.ts
@@ -0,0 +1,27 @@
+export interface FeatureFlags {
+	enableUpdateYDocFromDiskBuffer: boolean;
+	enableInvalidLinkDecoration: boolean;
+	enableDiffResolution: boolean;
+	enableDownloadOnAddToVault: boolean;
+	enableUploadOnShare: boolean;
+}
+
+export const FeatureFlagDefaults: FeatureFlags = {
+	enableUpdateYDocFromDiskBuffer: false,
+	enableInvalidLinkDecoration: false,
+	enableDiffResolution: false,
+	enableDownloadOnAddToVault: false,
+	enableUploadOnShare: false,
+};
+
+export type Flag = keyof FeatureFlags;
+
+// Autogenerate flag object which can be used to select flags like this...
+// withFlag(flag.enableInvalidLinkDecoration, () => {})
+type KeysToValues<T> = { [K in keyof T]: K };
+function createFlagObject<T>(): KeysToValues<T> {
+	return new Proxy({} as KeysToValues<T>, {
+		get: (target, prop) => prop,
+	});
+}
+export const flag = createFlagObject<FeatureFlags>();
