ba6d990b5814b959deb3d09921206f9483c291e2
Daniel Grossmann-Kavanagh <me@danielgk.com>
2024-09-12 10:26:39 -0700

    chore: Remove unused files

--- src/components/Checkbox.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	export let checked: boolean;
-	export let disabled: boolean;
-	export let label: string;
-</script>
-
-<div class="checkbox-container" aria-label={label}>
-	<input type="checkbox" tabindex="0" bind:checked {disabled} />
-</div>
