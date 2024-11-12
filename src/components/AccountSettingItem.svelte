4a667d777a4853d4d52101d272e3f050295bc1fb
Daniel Grossmann-Kavanagh <me@danielgk.com>
2024-11-12 10:10:01 -0800

    feat: Add avatar to settings page.

---
+++ src/components/AccountSettingItem.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { User } from "../User";
+	import Avatar from "./Avatar.svelte";
+
+	export let user: User;
+</script>
+
+<div class="setting-item">
+	<Avatar size="2.5em" user={user} />
+	<div class="setting-item-info" style="display: grid">
+		<div class="setting-item-description">
+			{user.name || ""} <br />
+			{user.email || ""}
+		</div>
+	</div>
+	<div class="setting-item-control">
+		<slot></slot>
+	</div>
+</div>
