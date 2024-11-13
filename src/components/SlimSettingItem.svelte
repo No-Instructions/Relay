bae932c434d887fdd2ed48b1d35d8fe0f0b99822
Daniel Grossmann-Kavanagh <me@danielgk.com>
2024-11-12 19:03:39 -0800

    fix: Improve settings page layout on mobile

---
+++ src/components/SlimSettingItem.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	export let name: HTMLSpanElement | string = "";
+	export let description: string;
+
+	const itemDescriptionLines =
+		description !== undefined ? description.split("\n") : [];
+</script>
+
+<div class="setting-item">
+	<div class="setting-item-info" style="display: grid">
+		<div class="setting-item-name" style="display: grid; align-items: center">
+			<slot name="name">{name}</slot>
+		</div>
+		<div class="setting-item-description">
+			<slot name="description">
+				{#if itemDescriptionLines.length > 0}
+					{#each itemDescriptionLines as itemDescriptionLine}
+						<div>
+							{itemDescriptionLine}
+						</div>
+					{/each}
+				{/if}
+			</slot>
+		</div>
+	</div>
+	<div class="setting-item-control">
+		<slot></slot>
+	</div>
+</div>
+
+<style>
+	.setting-item-control {
+		width: unset !important;
+		margin-top: unset !important;
+	}
+	.setting-item {
+		flex-direction: unset !important;
+		align-items: flex-start;
+	}
+</style>
