cffe9b115a91f9aedf99915dcbf285a4a9fa7159
Daniel Grossmann-Kavanagh <me@danielgk.com>
2024-11-14 11:24:14 -0800

    feat: New welcome page

--- src/components/LoggedIn.svelte
+++ src/components/LoggedIn.svelte
@@ -2,7 +2,8 @@
 	import { debounce, Notice, Platform } from "obsidian";
 	import type Live from "../main";
 	import GetInTouch from "./GetInTouch.svelte";
-	import SettingItem from "./SettingItem.svelte";
+	import WelcomeHeader from "./WelcomeHeader.svelte";
+	import WelcomeFooter from "./WelcomeFooter.svelte";
 	import AccountSettingItem from "./AccountSettingItem.svelte";
 	import SettingItemHeading from "./SettingItemHeading.svelte";
 	import Callout from "./Callout.svelte";
@@ -26,7 +27,6 @@
 	lm = plugin.loginManager;
 	let timedOut = writable<boolean>(false);
 	let success = writable<boolean>(false);
-	let showLink = writable<boolean>(false);
 	let useCustomFetch = writable<boolean>(true);
 
 	let url = writable<string>("please wait...");
@@ -129,10 +129,12 @@
 			.poll(provider, authWithCode)
 			.then((authRecord) => {
 				success.set(true);
+				pending.set(false);
 				error.set("");
 			})
 			.catch((e) => {
 				timedOut.set(true);
+				pending.set(false);
 				success.set(false);
 				error.set(e.message);
 			});
@@ -150,11 +152,11 @@
 	}
 </script>
 
-<SettingItemHeading>
-	<RelayText slot="name" />
-	<GetInTouch />
-</SettingItemHeading>
 {#if $lm.hasUser && $lm.user}
+	<SettingItemHeading>
+		<RelayText slot="name" />
+		<GetInTouch />
+	</SettingItemHeading>
 	<SettingItemHeading name="Account"></SettingItemHeading>
 	<AccountSettingItem user={$lm.user}>
 		<button
@@ -165,69 +167,143 @@
 	</AccountSettingItem>
 	<slot></slot>
 {:else}
-	<SettingItemHeading
-		name="Account"
-		helpText="We use Google login to help prevent spam sign-ups."
-	></SettingItemHeading>
-	{#if $automaticFlow}
-		{#if !$pending}
-			<SettingItem
-				name="Login"
-				description="You need to login to use this plugin."
+	<div class="welcome">
+		<WelcomeHeader />
+		{#if $automaticFlow}
+			<button
+				class="google-sign-in-button"
+				disabled={$pending}
+				on:click={debounce(async () => {
+					pending.set(true);
+					await login();
+				})}>Sign in with Google</button
 			>
-				<button
-					on:click={debounce(async () => {
-						await login();
-					})}>Login with Google</button
-				>
-			</SettingItem>
 		{:else}
-			<SettingItem
-				name="Login"
-				description="Please complete the login flow in your browser and wait a few seconds."
-			>
-				<button
-					class="mod-destructive"
-					on:click={debounce(() => {
-						pending.set(false);
-					})}>Cancel</button
-				>
-			</SettingItem>
-		{/if}
-	{:else}
-		{#if $error}
-			<Callout title="Error">
-				<p>{$error}</p>
-			</Callout>
-		{/if}
-		<SettingItem
-			name="Login"
-			description="Please complete the login flow in your browser and wait a few seconds."
-		>
 			<a href={$url} target="_blank">
 				<button
-					disabled={$url === "please wait..."}
+					class="google-sign-in-button"
+					disabled={$pending}
 					on:click={() => {
-						showLink.set(true);
+						pending.set(true);
 						poll();
-					}}>Login with Google</button
+					}}>Sign in with Google</button
 				>
 			</a>
-		</SettingItem>
-		{#if $timedOut}
-			<SettingItem
-				name="Check"
-				description="Click here once you've completed login"
-			>
-				<button
-					on:click={debounce(() => {
-						poll();
-					})}>Check</button
-				>
-			</SettingItem>
 		{/if}
-	{/if}
+		{#if $error}
+			<p>
+				{$error}.<br />
+				{#if $timedOut}
+					Already logged in? <button
+						class="link link-button"
+						on:click={debounce(() => {
+							poll();
+						})}>(click here)</button
+					>
+				{/if}
+			</p>
+		{:else if $pending}
+			<div>
+				<p class="continue">Continue in your browser...</p>
+				<p class="not-working">
+					Not working?
+					<button
+						class="link link-button"
+						on:click={() => {
+							pending.set(false);
+						}}>(try again)</button
+					>
+				</p>
+			</div>
+		{/if}
+	</div>
+	<WelcomeFooter />
 {/if}
 
 <style>
+	.continue {
+		font-weight: 600;
+		font-size: larger;
+		margin-top: 0;
+		margin-bottom: 0px;
+	}
+	.link {
+		color: var(--text-muted);
+	}
+
+	.link:hover {
+		color: var(--text-normal);
+	}
+
+	.not-working {
+		margin-top: 0px;
+		margin-bottom: 0px;
+		color: var(--text-faint);
+		font-size: 0.75em;
+	}
+
+	.link-button {
+		box-shadow: none;
+		background: none;
+		border: none;
+		padding: 0;
+		font: inherit;
+		text-decoration: underline;
+	}
+
+	button.link-button:hover {
+		box-shadow: none;
+		color: var(--text-normal);
+	}
+
+	button.link.link-button {
+		height: auto;
+		padding: 0;
+		color: var(--text-faint);
+	}
+
+	.welcome {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		text-align: center;
+		padding: 7rem 2rem 1rem 2rem;
+		max-width: 640px;
+		margin: 0 auto;
+		gap: 2rem;
+	}
+
+	.google-sign-in-button {
+		height: unset;
+		padding: 12px 16px 12px 42px !important;
+		border: none;
+		border-radius: 3px;
+		box-shadow:
+			0 -1px 0 rgba(0, 0, 0, 0.04),
+			0 1px 1px rgba(0, 0, 0, 0.25);
+		color: var(--text-color);
+		font-size: 14px;
+		font-weight: 500;
+		font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+			Ubuntu, Cantarell, "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif;
+		background-image: url(data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTgiIGhlaWdodD0iMTgiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PGcgZmlsbD0ibm9uZSIgZmlsbC1ydWxlPSJldmVub2RkIj48cGF0aCBkPSJNMTcuNiA5LjJsLS4xLTEuOEg5djMuNGg0LjhDMTMuNiAxMiAxMyAxMyAxMiAxMy42djIuMmgzYTguOCA4LjggMCAwIDAgMi42LTYuNnoiIGZpbGw9IiM0Mjg1RjQiIGZpbGwtcnVsZT0ibm9uemVybyIvPjxwYXRoIGQ9Ik05IDE4YzIuNCAwIDQuNS0uOCA2LTIuMmwtMy0yLjJhNS40IDUuNCAwIDAgMS04LTIuOUgxVjEzYTkgOSAwIDAgMCA4IDV6IiBmaWxsPSIjMzRBODUzIiBmaWxsLXJ1bGU9Im5vbnplcm8iLz48cGF0aCBkPSJNNCAxMC43YTUuNCA1LjQgMCAwIDEgMC0zLjRWNUgxYTkgOSAwIDAgMCAwIDhsMy0yLjN6IiBmaWxsPSIjRkJCQzA1IiBmaWxsLXJ1bGU9Im5vbnplcm8iLz48cGF0aCBkPSJNOSAzLjZjMS4zIDAgMi41LjQgMy40IDEuM0wxNSAyLjNBOSA5IDAgMCAwIDEgNWwzIDIuNGE1LjQgNS40IDAgMCAxIDUtMy43eiIgZmlsbD0iI0VBNDMzNSIgZmlsbC1ydWxlPSJub256ZXJvIi8+PHBhdGggZD0iTTAgMGgxOHYxOEgweiIvPjwvZz48L3N2Zz4=);
+		background-color: var(--background-secondary);
+		background-repeat: no-repeat;
+		background-position: 12px 11px;
+	}
+
+	.google-sign-in-button:hover {
+		box-shadow:
+			0 -1px 0 rgba(0, 0, 0, 0.04),
+			0 2px 4px rgba(0, 0, 0, 0.25);
+	}
+
+	.google-sign-in-button:disabled {
+		cursor: unset;
+		filter: grayscale(100%);
+		box-shadow:
+			0 -1px 0 rgba(0, 0, 0, 0.04),
+			0 1px 1px rgba(0, 0, 0, 0.25);
+	}
 </style>
