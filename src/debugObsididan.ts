a7c1e051d66c9d91016f175d650221a00e8f9456
Daniel Grossmann-Kavanagh <me@danielgk.com>
2024-12-10 14:19:49 -0800

    refactor: debug.ts no longer depends on Obsidian

---
+++ src/debugObsididan.ts
@@ -0,0 +1,36 @@
+import { Notice, Vault } from "obsidian";
+import type { IFileAdapter, INotifier } from "./debug";
+
+export class ObsidianNotifier implements INotifier {
+	notify(message: string) {
+		new Notice(message);
+	}
+}
+
+export class ObsidianFileAdapter implements IFileAdapter {
+	constructor(private vault: Vault) {}
+
+	async append(path: string, content: string) {
+		return this.vault.adapter.append(path, content);
+	}
+
+	async stat(path: string) {
+		return this.vault.adapter.stat(path);
+	}
+
+	async exists(path: string) {
+		return this.vault.adapter.exists(path);
+	}
+
+	async remove(path: string) {
+		return this.vault.adapter.remove(path);
+	}
+
+	async rename(oldPath: string, newPath: string) {
+		return this.vault.adapter.rename(oldPath, newPath);
+	}
+
+	async write(path: string, content: string) {
+		return this.vault.adapter.write(path, content);
+	}
+}
