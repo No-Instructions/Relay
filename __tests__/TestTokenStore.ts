a354bf9cf04d436f9ab4a321728ece1f7b087eb8
Daniel Grossmann-Kavanagh <me@danielgk.com>
2024-12-10 10:30:44 -0800

    refactor: factor out MockTimeProvider

--- __tests__/TestTokenStore.ts
+++ __tests__/TestTokenStore.ts
@@ -1,99 +1,6 @@
 import { TokenStore } from "../src/TokenStore";
-import type { TimeProvider } from "src/TimeProvider";
+import { MockTimeProvider } from "./mocks/MockTimeProvider";
 import { describe, expect, test } from "@jest/globals";
-
-class TestTimeProvider implements TimeProvider {
-	private currentTime: number;
-	private timers: Array<{
-		id: number;
-		callback: () => void;
-		triggerTime: number;
-	}> = [];
-	private nextTimerId = 0;
-
-	constructor() {
-		this.currentTime = Date.now();
-	}
-
-	getTime(): number {
-		return this.currentTime;
-	}
-
-	setTime(newTime: number): void {
-		const diff = (newTime - this.currentTime) / 1000;
-		console.log(`setting time to ${newTime} (+${diff}s)`);
-		this.currentTime = newTime;
-		this.checkTimers();
-	}
-
-	//setInterval(callback: () => void, ms: number): NodeJS.Timer {
-	//	const triggerTime = this.currentTime + ms;
-	//	const timerId = setTimeout(() => callback(), ms);
-	//	this.timers.push({ id: timerId, callback, triggerTime });
-	//	return timerId;
-	//}
-
-	setInterval(callback: () => void, ms: number): number {
-		return this.setTimeout(callback, ms, true);
-	}
-
-	clearInterval(timerId: number): void {
-		const id = <number>(<unknown>timerId);
-		this.timers = this.timers.filter((timer) => timer.id !== id);
-	}
-
-	setTimeout(callback: () => void, ms: number, isInterval = false): number {
-		const triggerTime = this.currentTime + ms;
-		const timerId = this.nextTimerId++;
-		const timer = { id: timerId, callback, triggerTime };
-		this.timers.push(timer);
-		if (isInterval) {
-			// If it's an interval, we immediately schedule the next execution
-			const index = this.timers.length - 1;
-			this.timers[index].callback = () => {
-				callback();
-				this.setTimeout(callback, ms, true); // Reschedule next execution
-			};
-		}
-		// eslint-disable-next-line @typescript-eslint/no-explicit-any
-		return <any>timerId;
-	}
-
-	destroy() {
-		this.timers.forEach((timer) => clearTimeout(timer.id));
-		this.timers = [];
-	}
-
-	//clearInterval(timerId: TimerID): void {
-	//	this.timers = this.timers.filter((timer) => timer.id !== timerId);
-	//	clearTimeout(timerId);
-	//}
-
-	//setTimeout(callback: () => void, ms: number): TimerID {
-	//	const triggerTime = this.currentTime + ms;
-	//	const timerId = { id: this.nextTimerId++ }; // Use an object as the ID to ensure uniqueness and avoid conflicts
-	//	this.timers.push({ id: timerId, callback, triggerTime });
-	//	return timerId;
-	//}
-
-	clearTimeout(timerId: number): void {
-		const id = <number>(<unknown>timerId);
-		this.timers = this.timers.filter((timer) => timer.id !== id);
-	}
-
-	private checkTimers(): void {
-		console.log(this.timers);
-		this.timers.forEach((timer) => {
-			if (this.currentTime >= timer.triggerTime) {
-				console.log("timer triggered");
-				timer.callback();
-				// eslint-disable-next-line @typescript-eslint/no-explicit-any
-				const id = <any>timer.id;
-				this.clearInterval(id);
-			}
-		});
-	}
-}
 
 interface TestToken {
 	token: string;
@@ -101,12 +8,12 @@ interface TestToken {
 
 async function _testTokenStore() {
 	// Setup
-	const testTimeProvider = new TestTimeProvider();
+	const testTimeProvider = new MockTimeProvider();
 	console.log(testTimeProvider);
 	const mockLog = (message: string) => console.log(`Log: ${message}`);
 	const mockRefresh = (
 		documentId: string,
-		callback: (newToken: TestToken) => void
+		callback: (newToken: TestToken) => void,
 	) => {
 		testTimeProvider.setTimeout(() => {
 			callback({
@@ -125,7 +32,7 @@ async function _testTokenStore() {
 			getTimeProvider: () => testTimeProvider,
 			getJwtExpiry: _testGetJwtExpiry,
 		},
-		1
+		1,
 	);
 
 	// Start the TokenStore processing
---
+++ __tests__/mocks/MockTimeProvider.ts
@@ -0,0 +1,94 @@
+import type { TimeProvider } from "src/TimeProvider";
+
+export class MockTimeProvider implements TimeProvider {
+	private currentTime: number;
+	private timers: Array<{
+		id: number;
+		callback: () => void;
+		triggerTime: number;
+	}> = [];
+	private nextTimerId = 0;
+
+	constructor() {
+		this.currentTime = Date.now();
+	}
+
+	getTime(): number {
+		return this.currentTime;
+	}
+
+	setTime(newTime: number): void {
+		const diff = (newTime - this.currentTime) / 1000;
+		console.log(`setting time to ${newTime} (+${diff}s)`);
+		this.currentTime = newTime;
+		this.checkTimers();
+	}
+
+	//setInterval(callback: () => void, ms: number): NodeJS.Timer {
+	//	const triggerTime = this.currentTime + ms;
+	//	const timerId = setTimeout(() => callback(), ms);
+	//	this.timers.push({ id: timerId, callback, triggerTime });
+	//	return timerId;
+	//}
+
+	setInterval(callback: () => void, ms: number): number {
+		return this.setTimeout(callback, ms, true);
+	}
+
+	clearInterval(timerId: number): void {
+		const id = <number>(<unknown>timerId);
+		this.timers = this.timers.filter((timer) => timer.id !== id);
+	}
+
+	setTimeout(callback: () => void, ms: number, isInterval = false): number {
+		const triggerTime = this.currentTime + ms;
+		const timerId = this.nextTimerId++;
+		const timer = { id: timerId, callback, triggerTime };
+		this.timers.push(timer);
+		if (isInterval) {
+			// If it's an interval, we immediately schedule the next execution
+			const index = this.timers.length - 1;
+			this.timers[index].callback = () => {
+				callback();
+				this.setTimeout(callback, ms, true); // Reschedule next execution
+			};
+		}
+		// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		return <any>timerId;
+	}
+
+	destroy() {
+		this.timers.forEach((timer) => clearTimeout(timer.id));
+		this.timers = [];
+	}
+
+	//clearInterval(timerId: TimerID): void {
+	//	this.timers = this.timers.filter((timer) => timer.id !== timerId);
+	//	clearTimeout(timerId);
+	//}
+
+	//setTimeout(callback: () => void, ms: number): TimerID {
+	//	const triggerTime = this.currentTime + ms;
+	//	const timerId = { id: this.nextTimerId++ }; // Use an object as the ID to ensure uniqueness and avoid conflicts
+	//	this.timers.push({ id: timerId, callback, triggerTime });
+	//	return timerId;
+	//}
+
+	clearTimeout(timerId: number): void {
+		const id = <number>(<unknown>timerId);
+		this.timers = this.timers.filter((timer) => timer.id !== id);
+	}
+
+	private checkTimers(): void {
+		console.log(this.timers);
+		this.timers.forEach((timer) => {
+			if (this.currentTime >= timer.triggerTime) {
+				console.log("timer triggered");
+				timer.callback();
+				// eslint-disable-next-line @typescript-eslint/no-explicit-any
+				const id = <any>timer.id;
+				this.clearInterval(id);
+			}
+		});
+	}
+}
