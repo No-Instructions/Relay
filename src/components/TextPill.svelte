6c2a266ce7542f688146bd116841e7983eed8397
Daniel Grossmann-Kavanagh <me@danielgk.com>
2025-02-04 13:30:42 -0800

    feat: Add NOT SYNCED tag to unsupported extensions in file tree

--- src/components/TextPill.svelte
+++ src/components/TextPill.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
 	export let text: string;
+	export let label: string;
 </script>
 
-<div class="nav-file-tag system3-filepill">
+<div class="nav-file-tag system3-filepill" aria-label={label}>
 	<span>{text}</span>
 </div>
 
