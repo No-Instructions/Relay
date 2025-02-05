5ef86b0cba9149ee7beb6076fda3046db4f38350
Daniel Grossmann-Kavanagh <me@danielgk.com>
2025-02-05 10:54:02 -0800

    feat: Migrate to using yjs observer events instead of updates

--- src/BackgroundSync.ts
+++ src/BackgroundSync.ts
@@ -174,8 +174,10 @@ export class BackgroundSync {
 			// Check for newly created documents without content, and reject them
 			const newDoc = new Y.Doc();
 			Y.applyUpdate(newDoc, updateBytes);
-			if (!newDoc.getText("contents").toString() && hasContents) {
-				this.log("[getDocument] server contents empty document, not overwriting local file.");
+			if (!newDoc.getText("contents").toString()) {
+				this.log(
+					"[getDocument] server contents empty document, not overwriting local file.",
+				);
 				return;
 			}
 
