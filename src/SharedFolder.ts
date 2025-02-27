b0399453fdd97e27faf0020169a4e4f81ee383d3
Daniel Grossmann-Kavanagh <me@danielgk.com>
2025-02-27 14:26:14 -0800

    fix: Enqueue uploads and downloads in createDoc

--- src/SharedFolder.ts
+++ src/SharedFolder.ts
@@ -894,6 +894,15 @@ export class SharedFolder extends HasProvider {
 		const doc =
 			this.docs.get(guid) || new Document(vpath, guid, this.loginManager, this);
 
+		if (doc.tfile?.stat.size === 0) {
+			// This might cause repeated download attempts for empty files?
+			this.backgroundSync.enqueueDownload(doc);
+		}
+
+		if (this.pendingUpload.get(doc.path)) {
+			this.backgroundSync.enqueueSync(doc);
+		}
+
 		this.docs.set(guid, doc);
 		this.docset.add(doc, update);
 
