85f176f806de3d9b55439ca63a6cfe75af9e1007
Daniel Grossmann-Kavanagh <me@danielgk.com>
2025-08-18 10:37:09 -0700

    feat: Kanban view support (behind a flag)

--- src/flags.ts
+++ src/flags.ts
@@ -17,6 +17,7 @@ export interface FeatureFlags {
 	enableGitHubLogin: boolean;
 	enableSelfManageHosts: boolean;
 	enableToasts: boolean;
+	enableKanbanView: boolean;
 }
 
 export const FeatureFlagDefaults: FeatureFlags = {
@@ -38,6 +39,7 @@ export const FeatureFlagDefaults: FeatureFlags = {
 	enableGitHubLogin: false,
 	enableSelfManageHosts: false,
 	enableToasts: false,
+	enableKanbanView: false,
 } as const;
 
 export function isKeyOfFeatureFlags(key: string): key is keyof FeatureFlags {
