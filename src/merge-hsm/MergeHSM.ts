049fd1f0281c74f6ac7194fceaff4667a2f88c08
Daniel Grossmann-Kavanagh <me@danielgk.com>
2026-08-07 10:35:09 -0700

    fix: settle conflict sync counters after resolution

--- src/merge-hsm/MergeHSM.ts
+++ src/merge-hsm/MergeHSM.ts
@@ -5965,6 +5965,7 @@ export class MergeHSM implements MachineHSM, SyncBridgeHost {
 		this._bridge.clearOutboundQueue();
 
 		this._fork = null;
+		this._bridge.resetPendingCounters();
 		this._ingestionTexts = [];
 		this._conflict = null;
 		// The user just settled this difference by hand. Any record that it was
